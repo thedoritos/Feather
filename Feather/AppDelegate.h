c4d4b7c4facc41661238eb2539511ad16fc461b1
thedoritos <thedoritos@gmail.com>
2015-02-05 22:02:22 +0900

    Create project

---
+++ Feather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Feather
+//
+//  Created by thedoritos on 2/5/15.
+//  Copyright (c) 2015 HumourStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
