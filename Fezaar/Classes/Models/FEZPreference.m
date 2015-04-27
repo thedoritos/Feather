5f546de9494ab79658e7702d988fdca8deb8bb0d
thedoritos <thedoritos@gmail.com>
2015-04-27 13:36:57 +0900

    Add preference DTO

---
+++ Fezaar/Classes/Models/FEZPreference.m
@@ -0,0 +1,38 @@
+//
+//  FEZPreference.m
+//  Fezaar
+//
+//  Created by t-matsumura on 4/27/15.
+//  Copyright (c) 2015 HumourStudio. All rights reserved.
+//
+
+#import "FEZPreference.h"
+
+static NSString * const kLastViewedListID = @"LastViewedListIDKey";
+
+@implementation FEZPreference
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+        
+        _lastViewedListID = [defaults objectForKey:kLastViewedListID];
+    }
+    return self;
+}
+
++ (instancetype)sharedPreference
+{
+    return [[FEZPreference alloc] init];
+}
+
+- (void)save
+{
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setValue:_lastViewedListID forKey:kLastViewedListID];
+    [defaults synchronize];
+}
+
+@end
