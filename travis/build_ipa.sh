25609b2bd5c3c45672fd2376fe19b64b5d41367c
thedoritos <thedoritos@gmail.com>
2015-04-30 22:30:08 +0900

    Fix identity

--- travis/build_ipa.sh
+++ travis/build_ipa.sh
@@ -7,5 +7,5 @@ fi
 #fi
 
 rm -rf ./build/*
-xcodebuild -workspace Fezaar.xcworkspace -scheme Fezaar -sdk iphoneos -configuration Release CODE_SIGN_IDENTITY='iOS Distribution: Tomohiro Matsumura' archive -archivePath ./build/Fezaar.xcarchive
+xcodebuild -workspace Fezaar.xcworkspace -scheme Fezaar -sdk iphoneos -configuration Release CODE_SIGN_IDENTITY='iPhone Distribution: Tomohiro Matsumura' archive -archivePath ./build/Fezaar.xcarchive
 xcodebuild -exportArchive -exportFormat IPA -archivePath ./build/Fezaar.xcarchive -exportPath ./build/Fezaar.ipa -exportProvisioningProfile 'Fezaar'
