e7e486bcc71ef66002672dededce22d562cc43c9
thedoritos <thedoritos@gmail.com>
2015-04-30 23:45:02 +0900

    Fix upload endpoint

--- travis/upload_ipa.sh
+++ travis/upload_ipa.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -F "file=@build/Fezaar.ipa" -F "token=$DEPLOY_GATE_API_KEY" https://deploygate.com/api/users/$DEPLOYGATE_USER_NAME/apps
+curl -F "file=@build/Fezaar.ipa" -F "token=$DEPLOY_GATE_API_KEY" https://deploygate.com/api/users/$DEPLOY_GATE_USER_NAME/apps
