e57cb0b9501517f1c0faa57d39d4f19c0ffe2cdb
thedoritos <thedoritos@gmail.com>
2015-05-03 21:36:15 +0900

    Add image only cell

---
+++ Fezaar/Classes/Views/Tweet/FEZImageCell/FEZImageCell.h
@@ -0,0 +1,16 @@
+//
+//  FEZImageCell.h
+//  Fezaar
+//
+//  Created by t-matsumura on 5/3/15.
+//  Copyright (c) 2015 HumourStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "FEZTweet.h"
+
+@interface FEZImageCell : UITableViewCell
+
+- (void)setTweet:(FEZTweet *)tweet;
+
+@end
---
+++ Fezaar/Classes/Views/Tweet/FEZImageCell/FEZImageCell.m
@@ -0,0 +1,46 @@
+//
+//  FEZImageCell.m
+//  Fezaar
+//
+//  Created by t-matsumura on 5/3/15.
+//  Copyright (c) 2015 HumourStudio. All rights reserved.
+//
+
+#import <SDWebImage/SDWebImageManager.h>
+#import "FEZImageCell.h"
+
+@interface FEZImageCell ()
+
+@property (weak, nonatomic) IBOutlet UIButton *imageButton;
+@property (nonatomic) FEZTweet *tweet;
+
+@end
+
+@implementation FEZImageCell
+
+- (void)awakeFromNib
+{
+    self.imageButton.imageView.contentMode = UIViewContentModeScaleAspectFill;
+    self.imageButton.imageView.clipsToBounds = YES;
+}
+
+- (void)setTweet:(FEZTweet *)tweet
+{
+    _tweet = tweet;
+    
+    if ([tweet containsMedia]) {
+        SDWebImageManager *manager = [SDWebImageManager sharedManager];
+        FEZMedia *media = tweet.entities.media.firstObject;
+        [manager downloadImageWithURL:media.mediaURL options:0 progress:nil completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, BOOL finished, NSURL *imageURL) {
+            
+            [self.imageButton setImage:image forState:UIControlStateNormal];
+        }];
+    }
+}
+
+- (IBAction)showImage:(id)sender
+{
+    [[NSNotificationCenter defaultCenter] postNotificationName:@"FEZNotificationShowMedia" object:nil userInfo:@{@"tweet" : self.tweet}];
+}
+
+@end
